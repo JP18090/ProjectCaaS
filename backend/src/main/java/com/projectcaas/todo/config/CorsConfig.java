3eb3dd0b0224bdaf240296a0a6dfd601bbd5034a
Gabriel Labarca Del Bianco <sdbianco10@gmail.com>
2026-03-15 23:46:40 -0300

    Update CorsConfig.java

--- backend/src/main/java/com/projectcaas/todo/config/CorsConfig.java
+++ backend/src/main/java/com/projectcaas/todo/config/CorsConfig.java
@@ -9,8 +9,15 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permite CORS em todos os endpoints
-                .allowedOrigins("*") // ACEITA QUALQUER IP E QUALQUER PORTA
+             registry.addMapping("/**")
+                    .allowedOrigins(
+                            "http://34.227.197.210",
+                            "http://localhost",
+                            "http://localhost:8080",
+                            "http://127.0.0.1",
+                            "http://127.0.0.1:8080"
+                    )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .maxAge(3600);
